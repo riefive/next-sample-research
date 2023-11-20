d4d4e7a1aa0efea963accd10c63b8a1271d30d95
riefive <rie.five@gmail.com>
2023-11-20 15:34:15 +0700

    chore: add demo skeleton and spinner

--- src/app/display-component/page.tsx
+++ src/app/display-component/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Pagination } from 'antd';
+import { Pagination, Skeleton, Space, Spin } from 'antd';
 import type { PaginationProps } from 'antd';
 
 const onShowSizeChange: PaginationProps['onShowSizeChange'] = (
@@ -12,7 +12,31 @@ const onShowSizeChange: PaginationProps['onShowSizeChange'] = (
 
 const DisplayComponentPage = () => {
   return (
-    <div className='p-4'>
+    <div className='flex flex-col gap-4 p-4'>
+      <Skeleton active></Skeleton>
+      <Space>
+        <h1>Shimmer 1</h1>
+        <Skeleton.Button size='default' shape='circle' block={true} />
+        <Skeleton.Avatar size='default' shape='circle' />
+        <Skeleton.Input size='default' block={true} />
+      </Space>
+      <Space>
+        <h1>Shimmer 2</h1>
+        <Skeleton.Button size='default' shape='square' block={true} />
+        <Skeleton.Avatar size='default' shape='square' />
+        <Skeleton.Input size='default' block={true} />
+      </Space>
+      <Space>
+        <h1>Shimmer 3</h1>
+        <Skeleton.Button size='default' shape='round' block={true} />
+        <Skeleton.Avatar size='default' shape='square' />
+        <Skeleton.Input size='default' block={true} />
+      </Space>
+      <Space>
+        <Spin />
+        <h1>Spinner</h1>
+      </Space>
+      <Pagination simple defaultCurrent={2} total={50} />
       <Pagination
         showSizeChanger
         onShowSizeChange={onShowSizeChange}
